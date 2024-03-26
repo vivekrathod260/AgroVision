0fe62a9612b19f1d5d45a1092be54cf96bffb287
Vivek <vivek.rathod@walchandsangli.ac.in>
2024-03-26 23:35:43 +0530

    renamed to AgroVision

--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ def main():
     """Run administrative tasks."""
     dotenv.load_dotenv()
     # dotenv.read_dotenv()
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AuthenticationLogin.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AgroVision.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
